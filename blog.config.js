0b3ee905b0a86fa916c050a57459fca0c5363bbb
ningjing <109914826+716ningjing@users.noreply.github.com>
2023-08-10 06:14:09 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,14 +13,14 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是路非专注于AIGC落地商业应用, 帮助企业降本增效,欢迎你来了解路非的AIGC课程，这是套上午学下午就能帮你降本增效的优秀课程，🎉',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是路非, 专注于AIGC落地商业应用帮助电商企业快速降本增效,欢迎你来了解路非的AIGC课程，这是套上午学下午就能帮电商企业降本增效的优秀课程，🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '路非AIGC课程', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '路非专注于AIGC落地商业应用，帮你的企业降本增效，我微信AIGCLF', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '路非的AIGC课程宣传页', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '路非致力于AIGC落地电商商业应用，帮电商企业降本增效，路非微信AIGCLF', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'www.aiwithgpt.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '路非, AIGC课程', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '路非X, AIGC课程', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
