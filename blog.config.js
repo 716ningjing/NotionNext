f04fd54fcb2a8f5ed9a9bc8fc96963d2a1a10c36
ningjing <109914826+716ningjing@users.noreply.github.com>
2023-08-10 16:19:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '路非的AIGC课程导览页', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '路非AIGC课程导览页', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '路非致力于AIGC落地电商商业应用，帮电商企业降本增效，路非微信AIGCLF', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'www.aiwithgpt.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '路非X, AIGC课程', // 网站关键词 英文逗号隔开
